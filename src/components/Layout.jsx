e32a2fb187ac46f3054f2435b0be47e5d40f7d9d
Alyssa <ca.butler10@gmail.com>
2019-02-17 16:28:01 +0100

    layout accidentally deleted

---
+++ src/components/Layout.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Header from './Header'
+import Footer from './Footer'
+import SEO from './SEO'
+import Tracking from './Tracking'
+
+function Layout({ isHomepage, children }) {
+  return (
+    <>
+      <SEO />
+      <Header isHomepage={isHomepage} />
+      {children}
+      <Footer />
+      <Tracking />
+    </>
+  )
+}
+NewProduct.propTypes = {
+  isHomepage: PropTypes.bool,
+  children: PropTypes.object.isRequired,
+}
+export default Layout
