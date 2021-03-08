f4514d14070d4826e7458621d8792948fd40d7a1
Alyssa <ca.butler10@gmail.com>
2021-03-08 08:23:39 +0100

    put back in linktree and recycled fins page

---
+++ src/components/linkTree/LinkTree.jsx
@@ -0,0 +1,156 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+import { makeStyles } from "@material-ui/styles"
+import Container from "../ui/Container"
+import logo from "../../assets/logos/logo.png"
+
+const useStyles = makeStyles(theme => ({
+  logo: {
+    width: 100,
+  },
+  h1: {
+    marginBottom: 30,
+  },
+  container: {
+    display: "flex",
+    justifyContent: "space-around",
+    flexWrap: "wrap",
+    width: "100%",
+  },
+  item: {
+    flexBasis: "100%",
+    textAlign: "center",
+    marginBottom: 30,
+    background: theme.color.black,
+    [theme.breakpoints.up("sm")]: {
+      flexBasis: "45%",
+      marginBottom: 45,
+    },
+  },
+  img: {
+    height: 400,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      height: 270,
+    },
+    [theme.breakpoints.up("md")]: {
+      height: 370,
+    },
+    [theme.breakpoints.up("lg")]: {
+      height: 500,
+    },
+  },
+  text: {
+    fontSize: 14,
+    color: theme.color.white,
+    "&:hover": {
+      color: theme.palette.primary.main,
+    },
+    [theme.breakpoints.up("md")]: {
+      fontSize: 18,
+      flexBasis: "49%",
+    },
+    [theme.breakpoints.up("lg")]: {
+      fontSize: 32,
+    },
+  },
+}))
+
+function LinkTree() {
+  const classes = useStyles()
+
+  const data = useStaticQuery(graphql`
+    query {
+      website: file(relativePath: { eq: "website.png" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid_tracedSVG
+          }
+        }
+      }
+      shop: file(
+        relativePath: { eq: "sustainable-surf-solutions-mobile-en.png" }
+      ) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid_tracedSVG
+          }
+        }
+      }
+      recycledFins: file(relativePath: { eq: "quickLockAD-mobile.png" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid_tracedSVG
+          }
+        }
+      }
+      onePercent: file(
+        relativePath: {
+          eq: "eisbach-riders-joins-1-percent-for-the-planet.png"
+        }
+      ) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid_tracedSVG
+          }
+        }
+      }
+    }
+  `)
+
+  const links = [
+    {
+      title: "Eisbach Riders",
+      img: "website",
+      link: "https://eisbach-riders.com/",
+    },
+    {
+      title: "SWS Shop",
+      img: "shop",
+      link: "https://secondwavesurfing.com/shop/",
+    },
+    {
+      title: "Recycled Quick-Lock Touring Fin",
+      img: "recycledFins",
+      link:
+        "https://secondwavesurfing.com/blog/eisbach-riders-a-touring-fin-made-of-recycled-waste-material-for-stand-up-paddling-with-the-quick-lock-system/",
+    },
+    {
+      title: "1% for the Planet",
+      img: "onePercent",
+      link:
+        "https://secondwavesurfing.com/blog/eisbach-riders-joins-1-percent-for-the-planet/",
+    },
+  ]
+
+  return (
+    <Container
+      justifyContent="center"
+      alignItems="center"
+      flexDirection="column"
+      padding="none"
+    >
+      <img src={logo} alt="logo" className={classes.logo}></img>
+      <h1 className={classes.h1}>Eisbach Riders</h1>
+      <div className={classes.container}>
+        {links.map((elem, idx) => (
+          <div className={classes.item} key={`link${idx}`}>
+            <a href={elem.link} target="_blank" rel="noopener">
+              <Img
+                fluid={data[elem.img].childImageSharp.fluid}
+                alt={elem.title}
+                placeholderStyle={{ backgroundColor: `white` }}
+                className={classes.img}
+                imgStyle={{ objectPosition: "center center" }}
+              />
+              <p className={classes.text}>{elem.title}</p>
+            </a>
+          </div>
+        ))}
+      </div>
+    </Container>
+  )
+}
+
+export default LinkTree
---
+++ src/components/recycledFins/RecycledFins.jsx
@@ -0,0 +1,269 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+import clsx from "clsx"
+import { useTranslation } from "react-i18next"
+import { makeStyles } from "@material-ui/core/styles"
+import Hidden from "@material-ui/core/Hidden"
+import Button from "@material-ui/core/Button"
+import Container from "../ui/Container"
+import BeerIcon from "../../assets/websiteImages/beer.svg"
+import RecycleIcon from "../../assets/websiteImages/recycle-sign.svg"
+import OnePercentIcon from "../../assets/websiteImages/1ftp_BusinessMember_Vertical_Black.png"
+import Paper from "@material-ui/core/Paper"
+import Timeline from "./Timeline"
+
+const useStyles = makeStyles(theme => ({
+  banner: {
+    width: "100%",
+  },
+  container: {
+    marginBottom: 100,
+  },
+  flex: {
+    display: "flex",
+    justifyContent: "space-between",
+    flexWrap: "wrap",
+    [theme.breakpoints.up("sm")]: {
+      flexWrap: "nowrap",
+    },
+    marginBottom: 100,
+  },
+  flexCenter: {
+    alignItems: "center",
+  },
+  reverse: {
+    flexDirection: "column-reverse",
+    [theme.breakpoints.up("sm")]: {
+      flexDirection: "row",
+    },
+  },
+  text: {
+    [theme.breakpoints.up("sm")]: {
+      flexBasis: "60%",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "space-between",
+      padding: 30,
+    },
+  },
+  img: {
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      flexBasis: "40%",
+      height: 500,
+    },
+  },
+  title: {
+    textTransform: "uppercase",
+    fontSize: 32,
+    letterSpacing: 3,
+    margin: 0,
+    marginBottom: 10,
+  },
+  right: {
+    textAlign: "right",
+  },
+  bottom: {
+    marginBottom: 45,
+  },
+  paper: {
+    background: theme.palette.primary.dark,
+    flexBasis: "100%",
+    padding: 15,
+    marginBottom: 30,
+    textAlign: "center",
+    [theme.breakpoints.up("sm")]: {
+      flexBasis: "30%",
+      marginBottom: 0,
+    },
+    [theme.breakpoints.up("lg")]: {
+      padding: 60,
+    },
+  },
+  icon: {
+    width: 100,
+    height: 132,
+    marginBottom: 15,
+    [theme.breakpoints.up("sm")]: {
+      marginBottom: 40,
+    },
+  },
+  cardTitle: {
+    textTransform: "uppercase",
+    fontSize: 18,
+    fontWeight: 700,
+    margin: 0,
+    [theme.breakpoints.up("lg")]: {
+      fontSize: 28,
+    },
+  },
+  question: {
+    fontWeight: 700,
+  },
+}))
+
+function RecycledFins() {
+  const classes = useStyles()
+  const { t } = useTranslation()
+  const images = useStaticQuery(graphql`
+    query {
+      shape: file(
+        relativePath: {
+          eq: "eisbach-riders-quick-lock-touring-fin-paddling-on-the-bodensee.jpg"
+        }
+      ) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      unsplash: file(relativePath: { eq: "kyle-hinkson-unsplash.jpg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      fileName: file(relativePath: { eq: "quickLockAD.png" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      mobile: file(relativePath: { eq: "quickLockAD-mobile.png" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Container>
+      <Hidden mdDown>
+        <Img
+          fluid={images.fileName.childImageSharp.fluid}
+          alt="quick lock ad"
+          placeholderStyle={{ backgroundColor: `white` }}
+          className={classes.banner}
+          imgStyle={{ objectPosition: "center center" }}
+        />
+      </Hidden>
+      <Hidden mdUp>
+        <Img
+          fluid={images.mobile.childImageSharp.fluid}
+          alt="quick lock ad"
+          placeholderStyle={{ backgroundColor: `white` }}
+          className={classes.banner}
+          imgStyle={{ objectPosition: "center center" }}
+        />
+      </Hidden>
+      <div className={clsx(classes.flex, classes.flexCenter, classes.reverse)}>
+        <Img
+          fluid={images.shape.childImageSharp.fluid}
+          alt="sup fin"
+          placeholderStyle={{ backgroundColor: `white` }}
+          className={classes.img}
+          imgStyle={{ objectPosition: "center center" }}
+        />
+        <div className={classes.text}>
+          <div>
+            <p className={classes.title}>{t("recycled.touringShape")}</p>
+            <p>{t("recycled.touringShapeInfo")}</p>
+            <p className={classes.bottom}>{t("recycled.touringStrokeInfo")}</p>
+          </div>
+          <div>
+            <p className={clsx(classes.right, classes.title)}>
+              {t("recycled.durability")}
+            </p>
+            <p className={clsx(classes.right, classes.bottom)}>
+              {t("recycled.durabilityInfo")}
+            </p>
+          </div>
+          <div>
+            <p className={classes.title}>{t("recycled.compatible")}</p>
+            <p className={classes.bottom}>{t("recycled.compatibleInfo")}</p>
+          </div>
+        </div>
+      </div>
+
+      <div className={classes.flex}>
+        <Paper className={classes.paper}>
+          <img src={BeerIcon} className={classes.icon} alt="germany icon" />
+          <p className={classes.cardTitle}>{t("recycled.madeInGermany")}</p>
+          <p className={classes.cardText}>{t("recycled.madeInGermanyInfo")}</p>
+        </Paper>
+        <Paper className={classes.paper}>
+          <img
+            src={OnePercentIcon}
+            className={classes.icon}
+            alt="1% for the planet logo"
+          />
+          <p className={classes.cardTitle}>{t("recycled.onePercentPlanet")}</p>
+          <p className={classes.cardText}>
+            {t("recycled.onePercentPlanetInfo")}
+          </p>
+        </Paper>
+        <Paper className={classes.paper}>
+          <img src={RecycleIcon} className={classes.icon} alt="recycle icon" />
+          <p className={classes.cardTitle}>{t("recycled.recycling")}</p>
+          <p className={classes.cardText}>{t("recycled.recyclingInfo")}</p>
+        </Paper>
+      </div>
+
+      <div className={classes.flex}>
+        <div className={classes.text}>
+          <p className={classes.title}>{t("recycled.projectHistory")}</p>
+          <p>{t("recycled.projectHistoryInfo1")}</p>
+          <p>{t("recycled.projectHistoryInfo2")}</p>
+          <p>{t("recycled.projectHistoryInfo3")}</p>
+        </div>
+        <Img
+          fluid={images.unsplash.childImageSharp.fluid}
+          alt="wave"
+          placeholderStyle={{ backgroundColor: `blue` }}
+          className={classes.img}
+          imgStyle={{ objectPosition: "center center" }}
+        />
+      </div>
+
+      <div className={classes.container}>
+        <p className={classes.title}>{t("recycled.timeline")}</p>
+        <p>{t("recycled.timelineInfo")}</p>
+        <Timeline />
+      </div>
+
+      <div className={classes.container}>
+        <p className={classes.title}>{t("links.faq")}</p>
+        <p className={classes.question}>{t("recycled.faq1")}</p>
+        <p>{t("recycled.faq1answer")}</p>
+        <p className={classes.question}>{t("recycled.faq2")}</p>
+        <p>{t("recycled.faq2answer")}</p>
+        <p className={classes.question}>{t("recycled.faq3")}</p>
+        <p>{t("recycled.faq3answer")}</p>
+        <p className={classes.question}>{t("recycled.faq4")}</p>
+        <p>{t("recycled.faq4answer")}</p>
+        <p className={classes.question}>{t("recycled.faq5")}</p>
+        <p>{t("recycled.faq5answer")}</p>
+        <Button
+          variant="contained"
+          color="primary"
+          href="https://secondwavesurfing.com/shop/product/sup-single-fin-us-box-touring/"
+          rel="noopener"
+          target="_blank"
+        >
+          {t("product.buy")}
+        </Button>
+        <p className={classes.question}>{t("recycled.faq6")}</p>
+        <p>{t("recycled.faq6answer")}</p>
+      </div>
+    </Container>
+  )
+}
+
+export default RecycledFins
